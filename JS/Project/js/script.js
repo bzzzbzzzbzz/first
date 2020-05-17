3a3b46cef526d61101798038448aeb45a6a4ed07
bzzzbzzzbzz <s.meln1k@ukr.net>
2020-05-18 01:17:19 +0200

    First commit

---
+++ JS/Project/js/script.js
@@ -0,0 +1,42 @@
+//'use strict';
+
+
+//var number = 5;
+//var string = "Hello";
+//var sym = Symbol();
+//var boolean = true;
+//var obj = {};
+
+
+/*let persone = {
+    name: "stas",
+    age: 20,
+    isMarried: true
+}*/
+
+/*console.log(persone.age);
+let arr = ['gg.png','fuck.png','gabela.png'];
+console.log(arr[0]);
+
+alert("Hello World");
+let answer = prompt("Are you sure?", "No");
+console.log(answer);
+console.log(4+"arr");*/
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+console.log(10%3);
+
+console.log("2" == 2);
+
+let isCheck = true,
+    isClose = false;
+console.log(isCheck || isClose)
+
